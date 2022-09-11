d96194c652576cf1fb69c1356150831140c15d20
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2022-09-11 18:36:19 -0300

    media queries e menu hamburguinho

--- mod4de5/ex026/mq005/JavaScript.js
+++ mod4de5/ex026/mq005/JavaScript.js
@@ -9,3 +9,15 @@ function navMenu () {
       menuClose.style.display = 'none';
    }
 }
+
+function mudouTamanho() {
+   if (window.innerWidth >= 768) {
+      menu.style.display = 'flex';
+      menuOpen.style.display = 'none';
+      menuClose.style.display = 'none';
+   } else {
+      menu.style.display = 'none';
+      menuOpen.style.display = 'flex';
+      menuClose.style.display = 'none';
+   }
+}
